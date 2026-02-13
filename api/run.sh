2effc13489d280e0656b4e59bb6a67e7b1a3043c
luohy15 <luohycs@gmail.com>
2026-02-13 21:57:45 +0800

    Fix import paths and add src to PYTHONPATH for Lambda runtime

--- api/run.sh
+++ api/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 PATH=$PATH:$LAMBDA_TASK_ROOT/bin \
-    PYTHONPATH=$PYTHONPATH:/opt/python:$LAMBDA_RUNTIME_DIR \
+    PYTHONPATH=$PYTHONPATH:/opt/python:$LAMBDA_RUNTIME_DIR:$LAMBDA_TASK_ROOT/src \
     exec python -m uvicorn --port=$PORT api.main:app
